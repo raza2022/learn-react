a909acdf773b40d9b6fe8a087726c808cb4de56d
Abdul Hameed <raza2022@gmail.com>
2017-02-23 17:04:28 +0500

    states

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,16 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todos: [
+        {id: 1, name: "learn react", isChecked: true },
+        {id: 2, name: "learn redux", isChecked: false },
+        {id: 3, name: "learn python", isChecked: false }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Todos</h2>
         </div>
-        <p className="App-intro">
-          Hello React first touch to react
-        </p>
+        <div className="todo-app">
+          <form action="">
+            <input type="text"/>
+          </form>
+          <div className="todo-list">
+            <ul>
+              {this.state.todos.map(todo => <li key={todo.id}>
+                <input type="checkbox" defaultChecked={todo.isChecked}/>{todo.name}
+              </li>)}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
